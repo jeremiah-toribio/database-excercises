2e669cc162029d4093d25984072c7ba3df757226
jeremiah-toribio <jeremiahtoribiotx@gmail.com>
2023-07-05 15:22:49 -0500

    added mysql_introduction

---
+++ mysql_introduction.sql
@@ -0,0 +1,6 @@
+-- 
+SHOW DATABASES;
+SELECT * FROM mysql.user;
+SELECT user, host FROM mysql.user;
+SELECT * FROM mysql.help_topic;
+SELECT help_topic_id, help_catagory_id, url FROM mysql.help_topic;
