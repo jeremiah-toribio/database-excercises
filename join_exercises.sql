5d590ad80ac4190c79515f1b6c9571eb67bb8640
jeremiah-toribio <jeremiahtoribiotx@gmail.com>
2023-07-11 09:26:24 -0500

    updated join ex

--- join_exercises.sql
+++ join_exercises.sql
@@ -40,54 +40,55 @@ limit 10;
 
 
 -- Q2.
-SELECT dept_name, CONCAT(e.first_name, ' ', e.last_name) AS manager_name
-FROM departments AS d
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS full_name, d.dept_name
+FROM employees AS e
 	JOIN dept_manager AS dm
-	ON d.dept_no = dm.dept_no
-    JOIN employees AS e
-    ON e.emp_no = dm.emp_no
-ORDER BY dept_name;
-
-
-
+		ON dm.emp_no = e.emp_no
+	JOIN departments AS d
+		ON d.dept_no = dm.dept_no
+	JOIN titles AS t
+		ON t.emp_no = e.emp_no
+WHERE t.to_date = '9999-01-01';
 
 -- Q3.
-SELECT dept_name, CONCAT(e.first_name, ' ', e.last_name) AS manager_name
-	FROM departments AS d
-    JOIN dept_manager AS dm
-    USING (dept_no)
-    JOIN employees as e
-    USING (emp_no)
-WHERE dm.to_date = '9999-01-01' AND e.gender = 'F';
+SELECT dept_name, CONCAT(e.first_name, ' ', e.last_name) AS full_name, t.title
+FROM employees AS e
+	JOIN dept_manager AS de
+		ON de.emp_no = e.emp_no
+	JOIN departments AS d
+		ON d.dept_no = de.dept_no
+	JOIN titles AS t
+		ON t.emp_no = e.emp_no
+WHERE t.to_date = '9999-01-01' AND e.gender = 'F';
 
 -- Q4.
 SELECT * from titles;
 
-SELECT t.title AS Title, COUNT(*) as Count
-FROM titles AS t
-	JOIN dept_emp AS de
-		USING (emp_no)
+SELECT t.title as Title, COUNT(t.title) as Count
+FROM employees AS e
+	JOIN dept_manager AS dm
+		ON dm.emp_no = e.emp_no
 	JOIN departments AS d
 		ON d.dept_no = dm.dept_no
-WHERE t.to_date = '9999-01-01' AND de.to_date = '9999-01-01'
-GROUP BY t.title
-ORDER BY t.title;
+	JOIN titles AS t
+		ON t.emp_no = e.emp_no
+	JOIN dept_emp AS de
+		ON de.emp_no = e.emp_no
+WHERE d.dept_no = 'd009' AND t.to_date = '9999-01-01'
+GROUP BY t.title;
 
+-- Q5. 
 
--- LEFT OFF HERE
--- Q5.
-
--- how to include dept name without error 
-SELECT dept_name, CONCAT(e.first_name, ' ', e.last_name) AS manager_name, salary
-FROM departments AS d
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS full_name, max(s.salary)
+FROM employees AS e
 	JOIN dept_manager AS dm
-		USING (dept_no)
-	JOIN salaries AS s
-		USING (emp_no)
-	JOIN employees AS e
-		USING (emp_no)
-WHERE s.to_date = '9999-01-01' AND dm.to_date = '9999-01-01'
-GROUP BY dept_name;
+		ON dm.emp_no = e.emp_no
+	JOIN departments AS d
+		ON d.dept_no = dm.dept_no
+	JOIN salaries as s
+		ON s.emp_no = e.emp_no
+WHERE s.to_date = '9999-01-01'
+GROUP BY e.emp_no;
 
 -- Q6.
 SELECT d.dept_no, d.dept_name, count(e.emp_no)
@@ -101,19 +102,17 @@ FROM employees AS e
 WHERE t.to_date = '9999-01-01'
 GROUP BY d.dept_no;
 
--- Q7.
-SELECT d.dept_name, AVG(s.salary) as average_salary
+-- Q7. Sales
+SELECT d.dept_name, AVG(s.salary)
 FROM departments as d
-	JOIN dept_emp as de
-		USING (dept_no)
-	JOIN salaries as s
-		USING (emp_no)
-WHERE t.to_date = '9999-01-01'
-GROUP BY dept_name
-ORDER BY average_salary desc
+	JOIN dept_emp AS de USING (dept_no)
+	JOIN salaries as s using (emp_no)
+WHERE s.to_date = '9999-01-01' and de.to_date = '9999-01-01'
+GROUP BY d.dept_name
+ORDER BY avg(s.salary) desc
 LIMIT 1;
 
--- Q8. Cristinel Bouloucos
+-- Q8. Akemi Warwick
 SELECT e.first_name, e.last_name
 FROM employees AS e
 	JOIN dept_emp AS de
@@ -128,26 +127,24 @@ WHERE t.to_date = '9999-01-01' AND s.to_date = '9999-01-01' AND d.dept_no = 'd00
 ORDER BY salary desc
 LIMIT 1;
 
--- Q9.
+-- Q9. Vishwani Minakawa
 select * from dept_manager;
 
-SELECT e.first_name, e.last_name, s.salary -- , d.dept_name
+SELECT first_name, last_name, salary, dept_name
 FROM departments as d
-	JOIN dept_manager AS dm
-		USING (dept_no)
-	JOIN salaries as s
-		USING (emp_no)
-	JOIN employees as
-		USING (emp_no)
-WHERE t.to_date = '9999-01-01'
+	join dept_manager as dm using (dept_no)
+    join salaries as s using (emp_no)
+    join employees as e using (emp_no)
+WHERE s.to_date = '9999-01-01' AND dm.to_date = '9999-01-01'
 ORDER BY salary desc
 LIMIT 1;
 
 -- Q10.
-SELECT d.dept_name, round(avg(s.salary)) as average_salary
+SELECT d.dept_name, avg(s.salary) as avg_salary
 from departments as d
 	join dept_emp as de on de.dept_no = d.dept_no
     join salaries as s on s.emp_no = de.emp_no
 group by d.dept_name
-order by average_salary desc;
+order by avg_salary;
 
+-- BONUS
